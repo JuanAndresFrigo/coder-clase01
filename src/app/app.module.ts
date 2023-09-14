ee4d1b57a8d22e8992774f9070bc15a03fb60cf3
Juan Andres Frigo <andres.frigo@fluxit.com.ar>
2023-09-13 22:58:28 -0300

    feat: trabajo realizado en la segunda clase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { PageWrapperComponent } from './page-wrapper/page-wrapper.component';
+import { StudentComponent } from './student/student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    ToolbarComponent,
+    SidebarComponent,
+    PageWrapperComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule,
